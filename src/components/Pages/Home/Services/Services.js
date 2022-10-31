eb029ddcc6bbf9cfe0b85cbc59b4d5473cf1e3f9
Masum Billah <masumbillah6584@gmail.com>
2022-10-31 22:30:18 +0600

    added more services btn

--- src/components/Pages/Home/Services/Services.js
+++ src/components/Pages/Home/Services/Services.js
@@ -13,17 +13,20 @@ const Services = () => {
     <div className="mt-16">
       <div className="text-center">
         <h3 className="text-2xl font-bold text-orange-500">Services</h3>
-        <h1 className="text-4xl font-extrabold">Our Service Area</h1>
+        <h1 className="text-4xl font-extrabold my-4">Our Service Area</h1>
         <p>
-          the majority have suffered alteration in some form, by injected
+          The majority have suffered alteration in some form, by injected
           humour, or randomised <br /> words which don't look even slightly
           believable.
         </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {services?.map((service, idx) => (
           <ServicesItem key={idx} service={service}></ServicesItem>
         ))}
+      </div>
+      <div className="text-center mt-12">
+        <button className="btn btn-outline btn-error">More Services</button>
       </div>
     </div>
   );
