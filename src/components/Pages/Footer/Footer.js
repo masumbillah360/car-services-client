54814f98bf03e9590ede4c7ff9a73ccec748ff15
Masum Billah <masumbillah6584@gmail.com>
2022-10-31 21:38:37 +0600

    Added footer and about us section

--- src/components/Pages/Footer/Footer.js
+++ src/components/Pages/Footer/Footer.js
@@ -1,10 +1,51 @@
 import React from "react";
+import logo from "../../../assets/logo.svg";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>This is Footer compo</h1>
-    </div>
+    <footer className="footer p-10 mt-10 bg-base-200 text-base-content">
+      <div>
+        <img src={logo} alt="" />
+        <p>
+          Edwin Diaz is a software and web <br />
+          technologies engineer, a life coach <br />
+          trainer who is also a serial .
+        </p>
+      </div>
+      <div>
+        <span className="footer-title">About</span>
+        <a href="/" className="link link-hover">
+          Home
+        </a>
+        <a href="/" className="link link-hover">
+          Services
+        </a>
+        <a href="/" className="link link-hover">
+          Contact
+        </a>
+      </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <a href="/" className="link link-hover">
+          Why Car Dorctor
+        </a>
+        <a href="/" className="link link-hover">
+          About
+        </a>
+      </div>
+      <div>
+        <span className="footer-title">Support</span>
+        <a href="/" className="link link-hover">
+          Support Center
+        </a>
+        <a href="/" className="link link-hover">
+          FeedBack
+        </a>
+        <a href="/" className="link link-hover">
+          Accesbility
+        </a>
+      </div>
+    </footer>
   );
 };
 
---
+++ src/components/Pages/Home/About/About.js
@@ -0,0 +1,48 @@
+import React from "react";
+import parts from "../../../../assets/images/about_us/parts.jpg";
+import person from "../../../../assets/images/about_us/person.jpg";
+
+const About = () => {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <div className="relative w-1/2">
+          <img
+            src={person}
+            alt=""
+            className="rounded-lg shadow-2xl w-4/5 h-full"
+          />
+          <img
+            src={parts}
+            alt="parts"
+            className="absolute top-1/2 right-5 2 w-3/5 border-8 rounded-lg border-white"
+          />
+        </div>
+        <div className="w-1/2">
+          <h3 className="text-xl font-bold text-orange-500">About US</h3>
+          <h1 className="text-5xl font-bold my-5">
+            We are qualified <br /> & of experience <br />
+            in this field
+          </h1>
+          <p className="p-0">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.
+          </p>
+          <p className="py-6">
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.
+          </p>
+
+          <button className="btn bg-orange-600 text-white font-bold">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
